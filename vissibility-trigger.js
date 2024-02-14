0ed5e2f26e03ed45aec02d14547adb7f81866964
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-02-14 15:18:23 +0400

    Add files via upload

---
+++ vissibility-trigger.js
@@ -0,0 +1,8 @@
+    function toggleMapLocation() {
+      var iframe = document.getElementById('mapIframe');
+      if (iframe.style.display === 'none') {
+        iframe.style.display = 'block';
+      } else {
+        iframe.style.display = 'none';
+      }
+    }
