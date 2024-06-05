9b9535f0771f80828332f09ef534589e237c7278
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-06-05 17:08:05 +0400

    Update vissibility-trigger.js

--- vissibility-trigger.js
+++ vissibility-trigger.js
@@ -1,4 +1,4 @@
-function toggleMapLocation() {
+  function toggleMapLocation() {
     var iframe = document.getElementById('mapIframe');
     if (iframe.style.display === 'none') {
         iframe.style.display = 'block';
@@ -6,4 +6,19 @@ function toggleMapLocation() {
         iframe.style.display = 'none';
     }
 }
+  document.getElementById('toggleButtonShowMore').addEventListener('click', function() {
+      const providers = document.querySelectorAll('.providers-list a:not(:nth-child(-n + 18))');
+      const isHidden = providers[0].style.display === 'none' || providers[0].style.display === '';
 
+      if (isHidden) {
+          providers.forEach(provider => {
+              provider.style.display = 'flex';
+          });
+          this.textContent = 'Show Less';
+      } else {
+          providers.forEach(provider => {
+              provider.style.display = 'none';
+          });
+          this.textContent = 'Show More';
+      }
+  });
