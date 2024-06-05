66e060de79a4d4019bf9779da766fc8288dad849
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-06-05 17:31:53 +0400

    Update vissibility-trigger.js

--- vissibility-trigger.js
+++ vissibility-trigger.js
@@ -6,22 +6,25 @@
         iframe.style.display = 'none';
     }
 }
+  function toggleProviders() {
+      const providers = document.querySelectorAll('.providers-list a:not(:nth-child(-n + 18))');
+      if (providers.length === 0) {
+          console.error('No providers found');
+          return;
+      }
+
+      const firstProvider = providers[0];
+      const isHidden = getComputedStyle(firstProvider).display === 'none';
+
+      providers.forEach(provider => {
+          provider.style.display = isHidden ? 'flex' : 'none';
+      });
+
+      const button = document.getElementById('toggleButtonShowMore');
+      button.textContent = isHidden ? 'Show Less' : 'Show More';
+  }
+
   document.addEventListener('DOMContentLoaded', function() {
       const button = document.getElementById('toggleButtonShowMore');
-      button.addEventListener('click', function() {
-          const providers = document.querySelectorAll('.providers-list a:not(:nth-child(-n + 18))');
-          if (providers.length === 0) {
-              console.error('No providers found');
-              return;
-          }
-
-          const firstProvider = providers[0];
-          const isHidden = getComputedStyle(firstProvider).display === 'none';
-
-          providers.forEach(provider => {
-              provider.style.display = isHidden ? 'flex' : 'none';
-          });
-
-          button.textContent = isHidden ? 'Show Less' : 'Show More';
-      });
+      button.addEventListener('click', toggleProviders);
   });
