e1815896d41e626a6e8d0f6a7b7a1d0d68d3a79f
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-06-05 17:14:47 +0400

    Update vissibility-trigger.js

--- vissibility-trigger.js
+++ vissibility-trigger.js
@@ -6,3 +6,16 @@
         iframe.style.display = 'none';
     }
 }
+  function toggleProviders() {
+      const providers = document.querySelectorAll('.providers-list a:not(:nth-child(-n + 18))');
+      const button = document.getElementById('toggleButtonShowMore');
+      const isHidden = providers[0].style.display === 'none' || providers[0].style.display === '';
+
+      providers.forEach(provider => {
+          provider.style.display = isHidden ? 'flex' : 'none';
+      });
+
+      button.textContent = isHidden ? 'Show Less' : 'Show More';
+  }
+
+  document.getElementById('toggleButtonShowMore').addEventListener('click', toggleProviders);
