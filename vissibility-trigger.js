41e7afb2de966509e90b9ba5d8007dda258625b0
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-06-05 17:54:36 +0400

    Update vissibility-trigger.js

--- vissibility-trigger.js
+++ vissibility-trigger.js
@@ -6,3 +6,22 @@
         iframe.style.display = 'none';
     }
 }
+        document.getElementById('toggleButtonShowMore').addEventListener('click', function() {
+            const button = this;
+            const styleText = `.providers-list a:not(:nth-child(-n + 18)) { display: flex; }`;
+            const existingStyle = document.getElementById('dynamicDisplayStyleForShowMore');
+
+            if (!existingStyle) {
+                // If style doesn't exist, add it
+                const style = document.createElement('style');
+                style.type = 'text/css';
+                style.id = 'dynamicDisplayStyleForShowMore';
+                style.textContent = styleText;
+                document.body.appendChild(style);
+                button.textContent = 'Show Less';
+            } else {
+                // If style exists, remove it
+                existingStyle.remove();
+                button.textContent = 'Show More';
+            }
+        });
