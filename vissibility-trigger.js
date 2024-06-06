c2f83236299ef12b7b29b5d63551115204261ac6
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-06-06 11:22:54 +0400

    Update vissibility-trigger.js

--- vissibility-trigger.js
+++ vissibility-trigger.js
@@ -6,20 +6,3 @@
         iframe.style.display = 'none';
     }
 }
-  function toggleProvidersMore() {
-    const desktopProviders = document.querySelectorAll('.desktop.home .providers-list a:not(:nth-child(-n + 18))');
-    const mobileProviders = document.querySelectorAll('.mobile.home .providers-list a:not(:nth-child(-n + 16))');
-    const button = document.querySelector('.show-more-button-switch');
-
-    const toggleDisplay = (providers) => {
-      providers.forEach(provider => {
-        const currentDisplay = window.getComputedStyle(provider).getPropertyValue('display');
-        provider.style.display = currentDisplay === 'none' ? 'flex' : 'none';
-      });
-    };
-
-    toggleDisplay(desktopProviders);
-    toggleDisplay(mobileProviders);
-
-    button.textContent = button.textContent === 'Show More' ? 'Show Less' : 'Show More';
-  }
