d787091f9a338644672037f262249fc08ea2415a
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-07-05 12:57:40 +0400

    Update vissibility-trigger.js

--- vissibility-trigger.js
+++ vissibility-trigger.js
@@ -6,28 +6,28 @@
         iframe.style.display = 'none';
     }
 }
-let currentIndex = 1;
-let slideInterval;
+  let currentIndex = 1;
+  let slideInterval;
 
-function showNextSlide() {
-  const iframesJp = [
-    document.getElementById('iframe3New'),
-    document.getElementById('iframe1Digital'),
-    document.getElementById('iframe2Amusnet')
-  ];
+  function showNextSlide() {
+    const iframesJp = [
+      document.getElementById('iframe1Digital'),
+      document.getElementById('iframe2Amusnet'),
+      document.getElementById('iframe3New')
+    ];
 
-  iframesJp[currentIndex].style.display = 'none';
-  currentIndex = (currentIndex + 1) % iframesJp.length;
-  iframesJp[currentIndex].style.display = 'block';
+    iframesJp[currentIndex].style.display = 'none';
+    currentIndex = (currentIndex + 1) % iframesJp.length;
+    iframesJp[currentIndex].style.display = 'block';
+
+    resetInterval();
+  }
+
+  function resetInterval() {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(showNextSlide, 16000);
+  }
+
+  window.showNextSlide = showNextSlide;
 
   resetInterval();
-}
-
-function resetInterval() {
-  clearInterval(slideInterval);
-  slideInterval = setInterval(showNextSlide, 16000);
-}
-
-window.showNextSlide = showNextSlide;
-
-resetInterval();
