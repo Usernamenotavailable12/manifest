66fe19daff586dc7f3c03a63dd2193a7be5b29f5
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-11-15 11:35:34 +0400

    Update vissibility-trigger.js

--- vissibility-trigger.js
+++ vissibility-trigger.js
@@ -1,4 +1,4 @@
-      function toggleMapLocation() {
+function toggleMapLocation() {
     var iframe = document.getElementById('mapIframe');
     if (iframe.style.display === 'none') {
         iframe.style.display = 'block';
@@ -8,42 +8,35 @@
 }
 let currentIndex = 0; // Start with the first iframe
 let slideInterval;
-
 function showNextSlide() {
   const iframesJp = [
+    document.getElementById('iframePNG'),
     document.getElementById('iframe2Amusnet'),
     document.getElementById('iframe3New'),
     document.getElementById('iframe1Digital')
   ];
-
   // Hide all iframes first
   iframesJp.forEach(iframe => iframe.style.display = 'none');
-
   // Show the next iframe
   currentIndex = (currentIndex + 1) % iframesJp.length;
   iframesJp[currentIndex].style.display = 'block';
-
   resetInterval();
 }
-
 function resetInterval() {
   clearInterval(slideInterval);
   slideInterval = setInterval(showNextSlide, 16000);
 }
-
 window.showNextSlide = showNextSlide;
-
 // Initial setup to ensure only the first iframe is visible
 document.addEventListener('DOMContentLoaded', () => {
   const iframesJp = [
+    document.getElementById('iframePNG'),
     document.getElementById('iframe2Amusnet'),
     document.getElementById('iframe3New'),
     document.getElementById('iframe1Digital')
   ];
-
   iframesJp.forEach((iframe, index) => {
     iframe.style.display = index === 0 ? 'block' : 'none';
   });
-
   resetInterval();
 });
