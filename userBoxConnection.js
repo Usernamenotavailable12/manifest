6378ff7d1f54fdfaaf4f39191e129d236a3f7afc
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-12-13 17:38:50 +0400

    Update userBoxConnection.js

--- userBoxConnection.js
+++ userBoxConnection.js
@@ -48,7 +48,7 @@ async function fetchActiveUserBoxes() {
 
     const query = `
     query UserBoxConnection($userId: ID, $brandId: ID!) {
-      userBoxConnection(userId: $userId, brandId: $brandId, last: 20) {
+      userBoxConnection(userId: $userId, brandId: $brandId, last: 40) {
         edges {
           node {
             userBoxId
